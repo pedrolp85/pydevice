f2fefe0e8cb8f376508b55942c1703e5e1be44fa
pedro <devops@python-test.localdomain.com>
2022-02-22 13:26:45 +0100

    test import

--- app/tests/unit/fixtures/test_file.py
+++ app/tests/unit/fixtures/test_file.py
@@ -1,21 +1,10 @@
-from repository.devices.DevicesRepositoryJSONFile import DevicesRepositoryJSONFile
+from app.repository.devices.devicesJSONfile import DevicesRepositoryJSONFile
 
-
-# class TestFileInput:
-#     def test_get_lines(self, mock_file, file_lines) -> None:
-#         file_input = FileInput("some_path.txt")
-#         result = list(file_input.get_lines())
-#         assert result == file_lines
 
 class TestDevices:
     def test_get_device(self, mock_file, file_device_json_content) -> None:
         devices_json = DevicesRepositoryJSONFile()
         result = devices_json.get_device(1)
-        assert result == file_lines
+        assert 0
 
 
-# class TestReversFileInput:
-#     def test_get_lines(self, mock_file, reverse_file_lines) -> None:
-#         file_input = ReverseFileInput("some_path.txt")
-#         result = list(file_input.get_lines())
-#         assert result == reverse_file_lines
