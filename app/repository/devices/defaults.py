4610aa4374abb66f6e84496d58ff9851e147d67c
pedro <devops@python-test.localdomain.com>
2022-02-24 19:29:47 +0100

    to revert

--- app/repository/devices/defaults.py
+++ app/repository/devices/defaults.py
@@ -3,8 +3,7 @@ from .devicesINIfile import DevicesRepositoryINIFile
 from .devicesJSONfile import DevicesRepositoryJSONFile
 from .devicesYAMLfile import DevicesRepositoryYAMLFile
 from settings import Settings, get_settings
-
-from fastapi import Depends
+from typing import Optional
 
 
 DEPENDENCY_RESOLVE_DICT = {
@@ -13,10 +12,13 @@ DEPENDENCY_RESOLVE_DICT = {
     "YAML": DevicesRepositoryYAMLFile,
 }
 
+def get_devices_repository(settings: Optional[Settings] = None) -> DevicesRepository:
 
-def get_devices_repository(settings: Settings = Depends(get_settings)) -> DevicesRepository:
-
+    settings = settings or get_settings()
+    print("AAAAAAAAAAAAAAAAAAAAAAAAA")
+    print(settings)
     if settings.inventory_source == "JSON":
+        print("DEVUELVE JSON")
         return DevicesRepositoryJSONFile(settings.inventory_file_source)
     if settings.inventory_source == "INI":
         return DevicesRepositoryINIFile(settings.inventory_file_source)
--- app/tests/integration/test_inventory/test_inventory_json.py
+++ app/tests/integration/test_inventory/test_inventory_json.py
@@ -10,33 +10,30 @@ import pytest
 
 client = TestClient(app)
 
+@pytest.fixture
+def mock_env_var_json() -> None:
+    with mock.patch.dict(os.environ, {"INVENTORY_SOURCE": "JSON", "INVENTORY_FILE_SOURCE": "tests/test_files/inventory"}):
+        yield
 
-def get_settings_override_json():
-    return Settings(inventory_source="JSON", inventory_file_source="tests/test_files/inventory")
 
-def test_get_devices_json():
-    app.dependency_overrides[get_settings] = get_settings_override_json
+def test_get_devices_json(mock_env_var_json):
     response = client.get("/device")
     assert response.status_code == 200
     assert len(response.json()) > 1
     assert response.json()[0]['name'] == "FW_INT1_json"
-    app.dependency_overrides = {}
 
-def test_get_device_json():
-    app.dependency_overrides[get_settings] = get_settings_override_json
+def test_get_device_json(mock_env_var_json):
     response = client.get("/device/2")
     assert response.status_code == 200
     assert response.json()['name'] == "BranchCSR1000v_json"
-    app.dependency_overrides = {}
 
-def test_create_device_json_successful():
-    app.dependency_overrides[get_settings] = get_settings_override_json
+def test_create_device_json_successful(mock_env_var_json):
     devices_raw = client.get("/device")
     devices =  [ Device(**p) for p in devices_raw.json() ]
     last_number = (devices[-1]).id
     response = client.post("/device",
     json={
-        "id": last_number+1 ,
+        "id": 100,
         "name": "FW_INT3_json",
         "manufacturer_id": 1,
         "model": "VM100",
@@ -44,6 +41,12 @@ def test_create_device_json_successful():
         "mgmt_interface_id":7
         },
     )
-    assert response.status_code == 200
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa")
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa")
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa")
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa")
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa")
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa")
+    print(response.json())
+    #assert response.status_code == 200
     assert response.json()['name'] == "FW_INT3_json"
-    app.dependency_overrides = {}
